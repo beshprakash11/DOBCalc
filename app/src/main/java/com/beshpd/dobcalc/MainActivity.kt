ecf0b03335fc53f54dc6597cbf40debc7ad3b034
beshprakash11 <beshprakashktm@gmail.com>
2023-02-25 10:19:59 +0100

    calendar properties added

--- app/src/main/java/com/beshpd/dobcalc/MainActivity.kt
+++ app/src/main/java/com/beshpd/dobcalc/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.beshpd.dobcalc
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.DatePicker
 import android.widget.Toast
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,13 +17,16 @@ class MainActivity : AppCompatActivity() {
 
         btnDatePicker.setOnClickListener{
             clickDatePicker()
-
         }
 
     }
 
     fun clickDatePicker(){
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        //DatePickerDialog()
         Toast.makeText(this, "btnDatePicker Pressed", Toast.LENGTH_SHORT).show()
-
     }
 }
