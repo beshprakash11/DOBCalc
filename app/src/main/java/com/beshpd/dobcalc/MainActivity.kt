7db51784155c22ff1d1253a02bc4e9d7b4ad9405
beshprakash11 <beshprakashktm@gmail.com>
2023-02-25 10:14:52 +0100

    btndatepicker updated and show data on pressed

--- app/src/main/java/com/beshpd/dobcalc/MainActivity.kt
+++ app/src/main/java/com/beshpd/dobcalc/MainActivity.kt
@@ -2,10 +2,19 @@ package com.beshpd.dobcalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker: Button = findViewById(R.id.btnDatePicker)
+
+        btnDatePicker.setOnClickListener{
+            Toast.makeText(this, "btnDatePicker Pressed", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
